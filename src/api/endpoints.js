a3511f5dec4092a48300e9f5d1bb648aaf690fd0
Rafael Willen <52548623+rafaelwillen@users.noreply.github.com>
2023-06-21 20:24:18 +0100

    Content add (#4)
    * Fix typescript error
    
    * Add content creation
    
    * format code

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -5,6 +5,19 @@ async function getAllContent() {
   return response;
 }
 
+async function uploadFiles(files) {
+  const formData = new FormData();
+  files.forEach((file) => formData.append("files", file));
+  const response = await axiosInstance.post("/file", formData);
+  return response.data;
+}
+
+async function createContent(data) {
+  await axiosInstance.post("/conteudo", data);
+}
+
 export default {
   getAllContent,
+  uploadFiles,
+  createContent,
 };
