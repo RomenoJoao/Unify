bd6371cdc776a9eac645181083b61543a3418f29
Rafael Willen <52548623+rafaelwillen@users.noreply.github.com>
2023-07-13 00:39:19 +0100

    Fix the sign up page (#7)

--- src/utils/user.js
+++ src/utils/user.js
@@ -3,24 +3,21 @@ import z from "zod";
 export const UserSchema = z.object({
   name: z
     .string()
-    .min(3, { message: "O nome deve ter pelo menos 3 caracteres" }),
+    .min(3,"O nome deve ter pelo menos 3 caracteres" ),
   lastname: z
     .string()
-    .min(3, { message: "O nome deve ter pelo menos 3 caracteres" }),
+    .min(3,"O nome deve ter pelo menos 3 caracteres" ),
   date: z.string().optional(),
-
-  login: z.object({
     email: z
       .string()
-      .email({ message: "Deve serEmail valido" })
+      .email("Deve serEmail valido" )
       .refine((vaue) => vaue.includes("@isptec.co.ao"), {
         message: "Deve ser email institucional",
       }),
-    password: z.string().min(6, { message: "Password invalida" }),
+    password: z.string().min(6,"Password invalida" ),
     role: z.string().optional(),
     username: z
       .string()
-      .min(6, { message: "Deve conter mais de 6 caracteres" })
+      .min(6,"Deve conter mais de 6 caracteres" )
       .optional(),
-  }),
 });
